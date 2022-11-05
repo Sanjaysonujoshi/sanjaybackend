11310606f552ecf97aead05003e94c2ab43b6e96
Sanjaysonujoshi <joshisonu305@gmail.com>
2022-11-06 00:56:00 +0530

    hy

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,33 @@
-const authenticate = function(req, req, next) {
+
+
+const authenticate = function(req, res, next) {
+  let info = req.header["x-auth-token"]
+   let token = jwt.verify(info,'functionup-thorium')
+  if(!token ){ return res.send({msg:"token is not valid"})
+}else{
+    next()
+}
+}
+
     //check the token in request header
     //validate this token
 
-    next()
-}
+    
+
+
 
 
 const authorise = function(req, res, next) {
+
+    let user = req.param.userId
+    let logginuser = decodedToken.userId
+
+    if(user != logginuser){
+        return res.send({msg:"is not correct user"})
+    }else{
+        next()
+    }
     // comapre the logged in user's id and the id in request
-    next()
+    
 }
+
